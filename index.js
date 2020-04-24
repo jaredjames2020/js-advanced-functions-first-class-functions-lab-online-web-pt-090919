3d066f080c00c1dc263d57f9d283226b00f3b195
Jared S <jaredjames2020@gmail.com>
2020-04-24 13:06:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
-  const firstTwoDrivers = drivers.filter(function(driver, index){
+  const returnfirstTwoDrivers = drivers.filter(function(driver, index){
       return console.log(index < 2);
   });
