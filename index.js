760b796b5295f05fc2fb63b0a126786469e6b37e
Jared S <jaredjames2020@gmail.com>
2020-04-24 13:17:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
  const returnFirstTwoDrivers = function(array) {
-    return array.slice(0, 2)
-}
+    return array.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(array) {
+    return array.slice(2,4)
+};
