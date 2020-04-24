8d291aed7ace1f8cc3d462354ea52d3116a4992b
Jared S <jaredjames2020@gmail.com>
2020-04-24 13:21:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ const returnLastTwoDrivers = function(array) {
     return array.slice(2,4);
 };
 
-const selectingDrivers = function(array) {
-    return (returnFirstTwoDrivers(array), returnLastTwoDrivers(array));
-};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
